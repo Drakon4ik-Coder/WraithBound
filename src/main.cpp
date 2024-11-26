821886e298c15cf6168eacbb4799ed881bc16403
Denis Skira <denis.skira@gmail.com>
2024-11-26 13:58:27 +0000

    main.cpp testing mode implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,29 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 
 int main(){
-  sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
-  sf::CircleShape shape(100.f);
-  shape.setFillColor(sf::Color::Green);
 
-  while (window.isOpen()){
-      sf::Event event;
-      while (window.pollEvent(event)){
-      if (event.type == sf::Event::Closed){
-        window.close();
-      }
+    const char* display = std::getenv("DISPLAY");
+    if (display == nullptr) {
+        std::cout << "Running in headless mode, skipping window creation." << std::endl;
     }
-    window.clear();
-    window.draw(shape);
-    window.display();
-  }
+    else {
+        sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
+        sf::CircleShape shape(100.f);
+        shape.setFillColor(sf::Color::Green);
+
+        while (window.isOpen()) {
+            sf::Event event;
+            while (window.pollEvent(event)) {
+                if (event.type == sf::Event::Closed) {
+                    window.close();
+                }
+            }
+            window.clear();
+            window.draw(shape);
+            window.display();
+        }
+    }
+  
   return 0;
 }
