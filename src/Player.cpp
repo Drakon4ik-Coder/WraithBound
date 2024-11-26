b2f96b04242e65a41ed46d6b6b720552c6abd6e6
Denis Skira <denis.skira@gmail.com>
2024-11-26 12:31:56 +0000

    Player cpp and header added

---
+++ src/Player.cpp
@@ -0,0 +1,25 @@
+#include "Player.h"
+#include <SFML/Window.hpp>
+
+using namespace sf;
+using namespace std;
+
+void Player::Update(double dt) {
+    // Move in four directions based on keys
+    if (Keyboard::isKeyPressed(Keyboard::W)) move(Vector2f(0, -_speed * dt));
+    if (Keyboard::isKeyPressed(Keyboard::S)) move(Vector2f(0, _speed * dt));
+    if (Keyboard::isKeyPressed(Keyboard::A)) move(Vector2f(-_speed * dt, 0));
+    if (Keyboard::isKeyPressed(Keyboard::D)) move(Vector2f(_speed * dt, 0));
+
+    Entity::Update(dt);
+}
+
+Player::Player()
+    : _speed(200.0f), Entity(make_unique<CircleShape>(25.f)) {
+    _shape->setFillColor(Color::Magenta);
+    _shape->setOrigin(Vector2f(25.f, 25.f));
+}
+
+void Player::Render(sf::RenderWindow& window) const {
+    window.draw(*_shape);
+}
