5cdc380d82cdf042ba01e31a19d50c7580c2477a
Illia <illiadovho@gmail.com>
2024-11-27 15:58:32 +0000

    Added sprites for melee and ranged monster

--- src/MeleeMonster.cpp
+++ src/MeleeMonster.cpp
@@ -6,7 +6,7 @@ void MeleeMonster::Update(const double dt) {
     Vector2f diff = player->getPosition() - getPosition();
     Vector2f direction = sf::normalize(diff);
     Vector2f moveVect = Vector2f(dt*_speed*direction.x, dt*_speed*direction.y);
-    if (length(diff) > 5) {
+    if (length(diff) > 30) {
         move(moveVect);
     }
     Monster::Update(dt);
